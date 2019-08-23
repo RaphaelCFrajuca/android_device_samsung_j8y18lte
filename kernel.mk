d8ed2ce1642cce1f8aee33d396b7e2d8c81b5b56
Raphael Frajuca <raphaelcfrajuca@gmail.com>
2019-08-23 02:48:02 -0300

    Some devide stuff

--- kernel.mk
+++ kernel.mk
@@ -20,3 +20,10 @@ BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom msm_rtb.filter=0x
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := SRPRD11A002KU
+TARGET_NO_BOOTLOADER := true
+
+# Crypto
+TARGET_HW_DISK_ENCRYPTION := true
